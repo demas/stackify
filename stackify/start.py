f3b6ed8b460b1b19656265e5c45b1a757fcf9402
Andrey Demidov <ceth@yandex-team.ru>
2020-04-12 08:44:08 +0300

    cli: module and tests

--- stackify/start.py
+++ stackify/start.py
@@ -8,6 +8,7 @@ import fetcher
 from store import get_database
 from fabulous.color import bg256, fg256
 import webbrowser
+from cli import Command, CommandType, Parser
 
 from ui import say_hello, display_list_of_questions, ls, alert_unclassified
 
@@ -18,40 +19,6 @@ classifier = Classifier(STOP_TAGS, FIRST_LEVEL_RULES)
 
 session = Session()
 say_hello()
-
-
-class CommandType:
-    EQUAL = "equal"
-    START_WITH = "start_with"
-    IN_LIST = "in_list"
-
-
-class Command:
-    def __init__(self, command_type: str, value: Union[str, List[str]], action):
-        self.command_type = command_type
-        self.value = value
-        self.action = action
-
-
-class Parser:
-    def __init__(self):
-        self.commands: List[Command] = []
-
-    def add_command(self, command: Command) -> None:
-        self.commands.append(command)
-
-    def process_message(self, message: str) -> None:
-        for command in self.commands:
-            if command.command_type == CommandType.EQUAL:
-                if message == command.value:
-                    return command.action()
-            elif command.command_type == CommandType.START_WITH:
-                if message.startswith(command.value):
-                    params = message[len(command.value):]
-                    return command.action(params)
-            elif command.command_type == CommandType.IN_LIST:
-                if message in command.value:
-                    return command.action(message)
 
 
 def fetch_action():
