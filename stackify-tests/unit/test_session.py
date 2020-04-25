6a3d1ef2980784bd9bef76b020720172433e2312
Andrey Demidov <ceth@yandex-team.ru>
2020-04-25 07:53:16 +0300

    tests for helpers and session

--- stackify-tests/unit/test_session.py
+++ stackify-tests/unit/test_session.py
@@ -1,3 +1,5 @@
+import pytest
+
 from session import ActiveSession
 
 
@@ -5,6 +7,10 @@ def test_create_session():
     session = ActiveSession()
 
     assert session is not None
+    assert len(session.all_questions) == 0
+    assert len(session.active_questions) == 0
+    assert len(session.current_tags) == 0
+    assert not session.last_used_tag
 
 
 def test_create_session_singleton():
@@ -14,3 +20,29 @@ def test_create_session_singleton():
     assert session1 is not None
     assert session2 is not None
     assert id(session1) == id(session2)
+
+
+@pytest.mark.parametrize("limit", [0, 1, 2, 3, 4, 5])
+def test_switch_to_tag(list_of_classified_questions, limit):
+    session = ActiveSession()
+
+    session.switch_to_tag("js", list_of_classified_questions, limit=limit)
+
+    assert session.all_questions == list_of_classified_questions
+    assert session.last_used_tag == "js"
+    assert (all(session.active_questions[i]["creation_date"] >= session.active_questions[i+1]["creation_date"]
+                for i in range(len(session.active_questions)-1)))
+    assert (all(session.all_questions[i]["creation_date"] >= session.all_questions[i+1]["creation_date"]
+                for i in range(len(session.all_questions)-1)))
+    assert len(session.active_questions) == min(len(list_of_classified_questions), limit)
+
+
+def test_remain_questions(list_of_questions):
+    session = ActiveSession()
+    session.switch_to_tag("js", list_of_questions, limit=1)
+
+    result = session.remain_questions()
+
+    assert len(result) == 2
+    assert (all(q not in result for q in session.active_questions))
+    assert len(session.active_questions + result) == 3
--- stackify/session.py
+++ stackify/session.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 class ActiveSession:
-    def __new__(cls):
+    def __new__(cls) -> "Session":
         if not hasattr(cls, "instance"):
             cls.instance = Session()
         return cls.instance
@@ -16,7 +16,7 @@ class Session:
         self.current_tags = []
 
     def switch_to_tag(self, tag: str, questions: List[dict], limit=8):
-        questions.sort(key=lambda q: -1 * q["creation_date"]) # TODO: test for sorting
+        questions.sort(key=lambda q: -1 * q["creation_date"])
         self.all_questions = questions
         self.active_questions = self.all_questions[:limit]
         self.last_used_tag = tag
