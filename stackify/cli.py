ded192d7b7be55f2ba786ca5b99722bbd71f011a
Andrey Demidov <ceth@yandex-team.ru>
2020-08-29 22:10:28 +0300

    refactore cli module

--- stackify/cli.py
+++ stackify/cli.py
@@ -1,6 +1,8 @@
 from typing import List
 from typing import Union
 
+POSITION_LIST = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
 
 class CommandType:
     EQUAL = "equal"
@@ -17,23 +19,17 @@ class Command:
 
 class Parser:
     def __init__(self, commands: List[Command] = None):
-        if commands:
-            self.commands = commands
-        else:
-            self.commands: List[Command] = []
+        self.commands = commands or []
 
     def add_command(self, command: Command) -> None:
         self.commands.append(command)
 
     def process_message(self, message: str) -> None:
         for command in self.commands:
-            if command.command_type == CommandType.EQUAL:
-                if message == command.value:
-                    return command.action()
-            elif command.command_type == CommandType.START_WITH:
-                if message.startswith(command.value):
-                    params = message[len(command.value):]
-                    return command.action(params)
-            elif command.command_type == CommandType.IN_LIST:
-                if message in command.value:
-                    return command.action(message)
+            if command.command_type == CommandType.EQUAL and message == command.value:
+                return command.action()
+            elif command.command_type == CommandType.START_WITH and message.startswith(command.value):
+                params = message[len(command.value):]
+                return command.action(params)
+            elif command.command_type == CommandType.IN_LIST and (message in command.value):
+                return command.action(message)
--- stackify/start.py
+++ stackify/start.py
@@ -2,19 +2,21 @@ import commands
 import ui
 from cli import Command
 from cli import CommandType
+from cli import POSITION_LIST
 from cli import Parser
 
 ui.say_hello()
-parser = Parser([Command(command_type=CommandType.EQUAL, value="f", action=commands.fetch_and_ls),
-                 Command(command_type=CommandType.START_WITH, value="ls", action=commands.show_questions_by_tag),
-                 Command(command_type=CommandType.START_WITH, value="s ", action=commands.show_questions),
-                 Command(command_type=CommandType.START_WITH, value="sn ", action=commands.show_new_questions),
-                 Command(command_type=CommandType.START_WITH, value="del", action=commands.delete_questions),
-                 Command(command_type=CommandType.START_WITH, value="hide", action=commands.hide_tag),
-                 Command(command_type=CommandType.START_WITH, value="unhide", action=commands.unhide_tag),
-                 Command(command_type=CommandType.IN_LIST, value=["1", "2", "3", "4", "5", "6", "7", "8", "9"],
-                         action=commands.open_question),
-                 Command(command_type=CommandType.START_WITH, value="o", action=commands.open_questions_list)])
+parser = Parser([
+    Command(command_type=CommandType.EQUAL, value="f", action=commands.fetch_and_ls),
+    Command(command_type=CommandType.START_WITH, value="ls", action=commands.show_questions_by_tag),
+    Command(command_type=CommandType.START_WITH, value="s ", action=commands.show_questions),
+    Command(command_type=CommandType.START_WITH, value="sn ", action=commands.show_new_questions),
+    Command(command_type=CommandType.START_WITH, value="del", action=commands.delete_questions),
+    Command(command_type=CommandType.START_WITH, value="hide", action=commands.hide_tag),
+    Command(command_type=CommandType.START_WITH, value="unhide", action=commands.unhide_tag),
+    Command(command_type=CommandType.IN_LIST, value=POSITION_LIST, action=commands.open_question),
+    Command(command_type=CommandType.START_WITH, value="o", action=commands.open_questions_list),
+])
 
 while True:
     message = input("> ")
