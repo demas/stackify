1ba0e76f97bd8d6772a6da47284730fec91c318e
Andrey Demidov <ceth@yandex-team.ru>
2020-10-04 16:56:19 +0300

    store refactoring

--- stackify-tests/funtest/conftest.py
+++ stackify-tests/funtest/conftest.py
@@ -1,27 +1,9 @@
-import random
-import sqlite3
-import string
-
-import pickledb
 import pytest
-import os
 
-from relation_store import create_tables, dict_factory
-from store import DB
+from relation_store import Connection
 
 
 @pytest.yield_fixture(scope="function")
-def testing_database():
-    filename = ''.join([random.choice(string.ascii_lowercase) for _ in range(15)])
-    db = DB(pickledb.load(filename, False))
-    yield db
-    os.remove(filename)
-
-
-@pytest.yield_fixture(scope="function")
-def testing_db_connection():
-    conn = sqlite3.connect(":memory:")
-    conn.row_factory = dict_factory
-    create_tables(conn)
-    return conn
+def test_connection():
+    return Connection(":memory:")
 
--- stackify-tests/funtest/test_relation_store.py
+++ stackify-tests/funtest/test_relation_store.py
@@ -1,48 +1,48 @@
-import random
 import sqlite3
-import string
 
-from relation_store import Connection, add_list_of_questions, add_question, delete_question, get_counts_by_category, \
-    get_questions, \
-    get_questions_by_category
+from relation_store import Connection
+from relation_store import add_list_of_questions
+from relation_store import add_question
+from relation_store import delete_question
+from relation_store import get_counts_by_category
+from relation_store import get_questions
+from relation_store import get_questions_by_category
 
 
 def test_connection():
-    filename = ''.join([random.choice(string.ascii_lowercase) for _ in range(15)])
-    connection = Connection(filename=filename)
+    connection = Connection(":memory:")
 
     assert connection is not None
     assert isinstance(connection, sqlite3.Connection)
 
 
 def test_connection_singleton():
-    filename = ''.join([random.choice(string.ascii_lowercase) for _ in range(15)])
-    connection1 = Connection(filename=filename)
-    connection2 = Connection(filename=filename)
+    connection1 = Connection(":memory:")
+    connection2 = Connection(":memory:")
 
     assert connection1 is not None
     assert connection2 is not None
     assert id(connection1) == id(connection2)
 
 
-def test_add_question(testing_db_connection, question_1):
+def test_add_question(test_connection, question_1):
     q = question_1
     q["category"] = "js"
     q["subcategory"] = "vue"
 
-    add_question(testing_db_connection, question_1)
+    add_question(test_connection, question_1)
 
-    result = get_questions(testing_db_connection)
+    result = get_questions(test_connection)
 
     # TODO: transformation
     #expected = q
     #assert result == [expected]
 
 
-def test_counts_by_category(testing_db_connection, list_of_classified_questions_with_different_categories):
-    add_list_of_questions(testing_db_connection, list_of_classified_questions_with_different_categories)
+def test_counts_by_category(test_connection, list_of_classified_questions_with_different_categories):
+    add_list_of_questions(test_connection, list_of_classified_questions_with_different_categories)
 
-    result = get_counts_by_category(testing_db_connection)
+    result = get_counts_by_category(test_connection)
 
     assert len(result) == 3
 
@@ -52,23 +52,23 @@ def test_counts_by_category(testing_db_connection, list_of_classified_questions_
 
 
 # TODO: better test
-def test_get_questions_by_category(testing_db_connection, list_of_classified_questions_with_different_categories):
-    add_list_of_questions(testing_db_connection, list_of_classified_questions_with_different_categories)
+def test_get_questions_by_category(test_connection, list_of_classified_questions_with_different_categories):
+    add_list_of_questions(test_connection, list_of_classified_questions_with_different_categories)
 
-    result = get_questions_by_category(testing_db_connection, "python")
+    result = get_questions_by_category(test_connection, "python")
 
     assert len(result) == 1
 
 
-def test_delete_question(testing_db_connection, question_1, question_2):
-    add_question(testing_db_connection, question_1)
-    add_question(testing_db_connection, question_2)
+def test_delete_question(test_connection, question_1, question_2):
+    add_question(test_connection, question_1)
+    add_question(test_connection, question_2)
 
-    questions = get_questions(testing_db_connection)
+    questions = get_questions(test_connection)
     assert len(questions) == 2
 
-    delete_question(testing_db_connection, question_1)
-    questions = get_questions(testing_db_connection)
+    delete_question(test_connection, question_1)
+    questions = get_questions(test_connection)
     assert len(questions) == 1
     assert questions[0]["question_id"] == question_2["question_id"]
 
